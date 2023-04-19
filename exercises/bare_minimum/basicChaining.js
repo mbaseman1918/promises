0beaabb0223737d30a3e07d90bfde73b5208853f
Matthew Baseman <matthew.baseman@gmail.com>
2023-04-18 17:12:28 -0700

    Complete Bare Minimum Requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -16,10 +16,13 @@ var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  // grabs the user name
   return prom.pluckFirstLineFromFileAsync(readFilePath)
+    //takes the user name and grabs the user profile
     .then(function(user) {
       return ific.getGitHubProfileAsync(user);
     })
+    // takes the user profile and writes it into a file
     .then(function(userprofile) {
       return writeFileAsync(writeFilePath, JSON.stringify(userprofile));
     });
